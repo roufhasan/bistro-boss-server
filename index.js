83254477fbc07cc318aa08c7d634c332191be90d
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2023-10-22 22:14:38 +0600

    carts get api created

--- index.js
+++ index.js
@@ -40,9 +40,18 @@ async function run() {
     });
 
     // Cart Collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const item = req.body;
-      console.log("reqest:", req);
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
