3718c2bd5e0d64d0c33eb2372f30a191a1b3fdae
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2023-10-24 18:09:11 +0600

    save sign up users and google login user's one time in user's db

--- index.js
+++ index.js
@@ -25,21 +25,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroBossDb").collection("users");
     const menuCollection = client.db("bistroBossDb").collection("menu");
     const reviewsCollection = client.db("bistroBossDb").collection("reviews");
     const cartCollection = client.db("bistroBossDb").collection("carts");
 
+    // User API's
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Menu API's
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    // Review API's
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
 
-    // Cart Collection
+    // Cart API's
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
       if (!email) {
